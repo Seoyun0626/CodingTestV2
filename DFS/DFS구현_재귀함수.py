84031b05c16cf0247f51a5732c253bbb83fca325
Seoyun0626 <tjdbs0626@gmail.com>
2024-03-19 20:57:23 +0900

    Update DFS구현_재귀함수.py
    예제 삭제

--- DFS/DFS구현_재귀함수.py
+++ DFS/DFS구현_재귀함수.py
@@ -1,21 +1,5 @@
 # DFS
 
-# 방문 정보를 리스트 자료형으로 표현
-visited = [False] * 9
-
-# 각 노드가 연결된 정보를 리스트 자료형으로 표현(2차원 리스트)
-graph = [
-    [],
-    [2, 3, 8],
-    [1, 7],
-    [1, 4, 5],
-    [3, 5],
-    [3, 4],
-    [7],
-    [2, 6, 8],
-    [1, 7]
-]
-
 def dfs(graph, v, visited):
     # 현재 노드 방문 처리
     visited[v] = True
@@ -25,5 +9,3 @@ def dfs(graph, v, visited):
         if not visited[i]:
             visited[i] = True
             dfs(graph, i, visited)
-
-dfs(graph,1, visited)

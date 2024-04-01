ee4625d188a16c55ca9570ec6a0a694d0aaafa45
Seoyun0626 <tjdbs0626@gmail.com>
2024-04-01 16:48:04 +0900

    [시간초과해결] 백준_14502_연구소
    벽을 세울 수 있는 경우의 수를 미리 combination으로 계산 후 bfs 돌리기

--- BFS/백준_14502_연구소.py
+++ BFS/백준_14502_연구소.py
@@ -1,6 +1,24 @@
 import copy
 import sys
 from collections import deque
+from itertools import combinations
+from copy import deepcopy
+
+def bfs(graph):
+    queue = deque([(j, i) for i in range(n) for j in range(m) if graph[i][j] == 2])
+
+    while queue:
+        x, y = queue.popleft()
+        for dy, dx in move:
+            if (0 <= x+dx < m) and (0 <= y+dy < n) and (graph[y+dy][x+dx] == 0):
+                queue.append((x+dx, y+dy))
+                graph[y+dy][x+dx] = 2
+    global answer
+    zeroCnt = 0
+    for cow in graph:
+        zeroCnt += cow.count(0)
+    answer = max(answer, zeroCnt)
+    # print(answer, zeroCnt)
 
 input = sys.stdin.readline
 move = [[1, 0], [-1, 0], [0, 1], [0, -1]]
@@ -8,45 +26,13 @@ n, m = map(int, input().split())
 graph = []
 for _ in range(n):
     graph.append(list(map(int, input().split())))
-# print(graph)
-
-def bfs():
-    queue = deque()
-    tmp_graph = copy.deepcopy(graph)
-
-    for y in range(n):
-        for x in range(m):
-            if graph[y][x] == 2:
-                queue.append((x, y))
-    while queue:
-        x, y = queue.popleft()
-        for dy, dx in move:
-            if 0 <= x+dx < m and 0 <= y+dy < n:
-                if tmp_graph[y+dy][x+dx] == 0:
-                    queue.append((x+dx, y+dy))
-                    tmp_graph[y+dy][x+dx] = 2
-    global answer
-    zeroCnt = 0
-    for cow in tmp_graph:
-        zeroCnt += cow.count(0)
-    answer = max(answer, zeroCnt)
-
-def makeWall(cnt):
-
-    # 벽 3개를 세우면 바이러스 퍼뜨리기
-    if cnt == 3:
-        bfs()
-        return
-
-    for y in range(n):
-        for x in range(m):
-            if graph[y][x] == 0:
-                # 벽 세우기
-                graph[y][x] = 1
-                makeWall(cnt+1)
-                # 최적해가 아니면 벽 세운거 허물기
-                graph[y][x] = 0
-
+x_y = [(x, y) for x in range(m) for y in range(n) if graph[y][x] == 0]
 answer = 0
-makeWall(0)
+
+# 벽 세울 수 있는 조합 구성
+for c in combinations(x_y, 3):
+    tmp_graph = deepcopy(graph)
+    for x, y in c:
+        tmp_graph[y][x] = 1
+    bfs(tmp_graph)
 print(answer)

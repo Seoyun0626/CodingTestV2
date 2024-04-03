ed7ff2577acd86624287fec2c9112d49c6066fea
Seoyun0626 <tjdbs0626@gmail.com>
2024-04-03 17:11:40 +0900

    Update 백준_18111_마인크래프트.py
    [시간초과해결]
    graph = 땅을 입력 받기X (2중 for문)
    graph = 높이에 따른 땅의 개수 (for문 한번)

--- 구현/백준_18111_마인크래프트.py
+++ 구현/백준_18111_마인크래프트.py
@@ -1,42 +1,50 @@
-# 세로(y), 가로(x)
-# 땅의 높이 일정하게 바꾸기
-# 블록 제거 -> 2초, 블록 쌓기 -> 1초
 import sys
 input = sys.stdin.readline
 
 n, m, b = map(int, input().split())
-graph = []
-dic = {}
-for _ in range(n):
-    graph.append(list(map(int, input().split())))
+## 높이별 땅의 개수
+graph = [0] * 257
+time = sys.maxsize - 1
+height = 0
+
+## 시간 구하는 함수
+def solution(goal):
+    # 추가 블록
+    need = 0
+    # 제거 블록
+    remove = 0
+    for i in range(257):
+        # i = 현재 높이
+        # 현재 높이의 개수, 만드려는 높이 차
+        current_num, diff = graph[i], i - goal
+        if current_num == 0:
+            continue
+        ## 블록을 추가해야함
+        if diff < 0:
+            need += (-diff) * current_num
+        ## 블록 제거
+        else:
+            remove += (diff) * current_num
+    if remove + b >= need:
+        time = need + 2 * remove
+        return time
+    else:
+        return sys.maxsize
+
+
+## 땅에 대한 높이를 2차원 배열이 아닌 1차원 배열로 만들기
+for i in range(n):
+    for j in list(map(int, input().split())):
+        graph[j] += 1
 # print(graph)
 
-for goal in range(0, 256):
-    time = 0
-    block = b
-    flag = 0
-    for y in range(n):
-        for x in range(m):
-            if graph[y][x] == goal:
-                continue
-            ## 블록이 많아서 제거
-            elif graph[y][x] > goal:
-                remove_block = graph[y][x] - goal
-                block += remove_block
-                time += (2 * remove_block)
-            else:
-                more_block = goal - graph[y][x]
-                if more_block > block:
-                    flag = 1
-                    break
-                block -= more_block
-                time += (1 * more_block)
-    if flag == 1:
-        break
-    else:
-        dic[goal] = time
-dic = dict(sorted(dic.items(), key = lambda x : x[1]))
-result = list(dic.items())[0]
-print(result[1], result[0])
+for goal in range(257):
+    time_value = solution(goal)
+    if time >= time_value:
+        time = time_value
+        height = goal
+print(time, height)
+
+
 
 

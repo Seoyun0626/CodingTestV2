a8b5cf9496742985a854cbaa219897a3126ed75a
Seoyun0626 <tjdbs0626@gmail.com>
2024-04-03 13:13:53 +0900

    Update 백준_2167_2차원 배열의 합.py
    DP 사용 코드

--- 구현/백준_2167_2차원 배열의 합.py
+++ 구현/백준_2167_2차원 배열의 합.py
@@ -3,15 +3,17 @@ import sys
 input = sys.stdin.readline
 n, m = map(int, input().split())
 matrix = []
-result = 0
 for _ in range(n):
     matrix.append(list(map(int, input().split())))
-# for cow in matrix:
-    # print(cow)
 k = int(input())
-for _ in range(k):
-    result = 0
-    left_y, left_x, right_y, right_x = map(int, input().split())
-    for y in range(left_y - 1, right_y):
-        result += sum(matrix[y][left_x - 1:right_x])
-    print(result)
+do = [list(map(int, input().split())) for _ in range(k)]
+# print(do)
+
+dp = [[0 for i in range(m + 1)] for _ in range(n + 1)]
+for y in range(1, n + 1):
+    for x in range(1, m + 1):
+        dp[y][x] = dp[y - 1][x] + dp[y][x - 1] - dp[y - 1][x - 1] + matrix[y - 1][x - 1]
+
+for _, line in enumerate(do):
+    j, i, y, x = line
+    print(dp[y][x] - (dp[y][i - 1] + dp[j - 1][x]) + dp[j - 1][i - 1])

eb8e98031b2ce412a4e5ce3ac20b753ca530ae7f
Seoyun0626 <tjdbs0626@gmail.com>
2024-04-03 12:36:48 +0900

    Create 백준_2167_2차원 배열의 합.py
    [시간초과] : for문 2번 사용

---
+++ 구현/백준_2167_2차원 배열의 합.py
@@ -0,0 +1,18 @@
+# i == y, j == x
+import sys
+input = sys.stdin.readline
+n, m = map(int, input().split())
+matrix = []
+result = 0
+for _ in range(n):
+    matrix.append(list(map(int, input().split())))
+# for cow in matrix:
+    # print(cow)
+k = int(input())
+for _ in range(k):
+    result = 0
+    left_y, left_x, right_y, right_x = map(int, input().split())
+    for y in range(left_y - 1, right_y):
+        for x in range(left_x - 1, right_x):
+            result += matrix[y][x]
+    print(result)

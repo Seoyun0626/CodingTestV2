acccd6fd6e188c2d18c73fdc1c4bc9748fd6eb83
Seoyun0626 <tjdbs0626@gmail.com>
2024-04-24 13:57:31 +0900

    Update [백준]11000_강의실 배정.py
    최소힙 heapq 사용 -> O(logN) -> 시간초과 해결 가능

--- 이코테/그리디/[백준]11000_강의실 배정.py
+++ 이코테/그리디/[백준]11000_강의실 배정.py
@@ -1,22 +1,24 @@
+import sys
+import heapq
+
+input = sys.stdin.readline
 n = int(input())
 info = []
 room = []
 for _ in range(n):
     start, final = map(int, input().split())
     info.append((start, final))
+info.sort() # 시작 시간 기준 정렬
 # print(info)
 
-for s,t in info:
-    flag = 0
-    if len(room) == 0:
-        room.append([s, t])
+heapq.heappush(room, info[0][1])
+
+for i in range(1, len(info)):
+    s, t = info[i][0], info[i][1]
+    # room은 heapq구현 -> room[0] 항상 최솟값
+    if room[0] <= s:
+        heapq.heappop(room)
+        heapq.heappush(room, t)
     else:
-        for tmp_room in room:
-            final = tmp_room[-1]
-            if final <= s:
-                flag = 1
-                tmp_room.append(t)
-                break
-        if flag == 0:
-            room.append([s, t])
+        heapq.heappush(room, t)
 print(len(room))
